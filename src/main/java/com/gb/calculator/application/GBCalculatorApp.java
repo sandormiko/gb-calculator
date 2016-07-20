8a68865b8d125a3d7e888cbd9a51bae18fa784cb
Sándor Mikó <miko@elte.hu>
2016-07-20 23:54:07 +0200

    Initial gradle build scripts

---
+++ src/main/java/com/gb/calculator/application/GBCalculatorApp.java
@@ -0,0 +1,16 @@
+package com.gb.calculator.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication(scanBasePackages="com.gb.calculator")
+public class GBCalculatorApp {
+	
+		public static void main(String[] args) {
+			SpringApplication.run(GBCalculatorApp.class,args); 
+
+		}
+
+	}
+	
