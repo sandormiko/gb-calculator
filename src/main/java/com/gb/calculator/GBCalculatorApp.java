87ed3fe3c5070df40dbfe4b3f805d58e43b6a8f6
Sándor Mikó <miko@elte.hu>
2016-07-21 00:42:37 +0200

    Set java version for Travis

--- src/main/java/com/gb/calculator/GBCalculatorApp.java
+++ src/main/java/com/gb/calculator/GBCalculatorApp.java
@@ -1,16 +1,25 @@
-package com.gb.calculator.application;
+package com.gb.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
 
 
-@SpringBootApplication(scanBasePackages="com.gb.calculator")
+@SpringBootApplication(exclude=DispatcherServletAutoConfiguration.class,scanBasePackages="com.gb.calculator")
 public class GBCalculatorApp {
 	
 		public static void main(String[] args) {
 			SpringApplication.run(GBCalculatorApp.class,args); 
 
 		}
+		
+		@Bean
+	    public ServletRegistrationBean dispatcherServlet() {
+	        return new ServletRegistrationBean(new DispatcherServlet(), "/calculator-rs/*");
+	    }
 
 	}
 	
