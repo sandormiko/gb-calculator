fb756a0d8ac75da271c0eccc17f8d73712ffebbe
Sándor Mikó <miko@elte.hu>
2016-07-31 23:48:38 +0200

    Make rest endpoint work

--- src/main/java/com/gb/calculator/GBCalculatorApp.java
+++ src/main/java/com/gb/calculator/GBCalculatorApp.java
@@ -2,13 +2,9 @@ package com.gb.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.DispatcherServlet;
 
 
-@SpringBootApplication(exclude=DispatcherServletAutoConfiguration.class,scanBasePackages="com.gb.calculator")
+@SpringBootApplication
 public class GBCalculatorApp {
 	
 		public static void main(String[] args) {
@@ -16,10 +12,6 @@ public class GBCalculatorApp {
 
 		}
 		
-		@Bean
-	    public ServletRegistrationBean dispatcherServlet() {
-	        return new ServletRegistrationBean(new DispatcherServlet(), "/calculator-rs/*");
-	    }
 
 	}
 	
