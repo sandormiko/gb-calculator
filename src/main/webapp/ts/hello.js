7c5ea07a5f02765a98dae57be4a5cd009c3abb1e
Sándor Mikó <miko@elte.hu>
2016-07-31 23:35:36 +0200

    Added typescript transpilation

---
+++ src/main/webapp/ts/hello.js
@@ -0,0 +1,11 @@
+var Greeter = (function () {
+    function Greeter(greeting) {
+        this.greeting = greeting;
+    }
+    Greeter.prototype.greet = function () {
+        return "<h1>" + this.greeting + "</h1>";
+    };
+    return Greeter;
+}());
+;
+var greeter = new Greeter("2");
