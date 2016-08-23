8702e0375b2876f3d5d24670ec0114c890e8828e
Sándor Mikó <miko@elte.hu>
2016-08-23 07:12:32 +0200

    Include mocha tests in build lifecycle

---
+++ src/main/resources/static/test/statistics/statistics.component.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+
+describe('Statistics', () => {
+  it('true should be equal to true', () => {
+
+
+    // Chai assertion.
+    expect(true).to.be.equal(false);
+  });
+});
